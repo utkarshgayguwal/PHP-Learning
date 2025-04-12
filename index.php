32179e7cf87f4328522fd2559897b2ebcf665983
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2025-04-12 23:15:20 +0530

    ---Start of Section 2: Dynamic Web Application---
    - Added UI template from tailwind ui website
    - Added partials for particular sections like header, navbar, banner, and footer.
    - Banner partial is dynamic.

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-$books = [
-    [
-        'name'        => 'Do Androids Dream of Electric Sheep',
-        'author'      => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'http://example.com',
-    ],
-    [
-        'name'        => 'Project Hail Mary',
-        'author'      => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'http://example.com',
-    ],
-    [
-        'name'        => 'The Martian',
-        'author'      => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'http://example.com',
-    ],
-];
+$heading = "Home";
 
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['author'] === 'Andy Weir';
-});
-
-require 'index.view.php';
+require "views/index.view.php";
