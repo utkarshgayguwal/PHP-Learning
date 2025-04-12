4f293182de19316ca094e6ca4295c0ba1f330613
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2025-04-12 20:43:43 +0530

    Php Built-in array_filter function

--- index.php
+++ index.php
@@ -28,20 +28,7 @@
             ],
         ];
 
-        $filter = function ($items, $fn)
-        {
-            $filteredItems = [];
-
-            foreach ($items as $item) {
-                if ($fn($item)) {
-                    $filteredItems[] = $item;
-                }
-            }
-
-            return $filteredItems;
-        };
-
-        $filteredBooks = $filter($books, function ($book){
+        $filteredBooks = array_filter($books, function ($book){
             return $book['author'] === 'Andy Weir';
         });
     ?>
