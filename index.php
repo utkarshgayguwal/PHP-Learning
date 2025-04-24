38fdfdc9cf3e1c4064d8971ecbb9ba1a6952c80c
Utkarsh Gayguwal <utkarshgayguwal.tg@gmail.com>
2025-04-24 16:45:08 +0000

    Data fetching from db using PDO.

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
 require "function.php";
+$dsn = "mysql:host=localhost;port=3306;dbname=php_beginners;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root', 'password');
+// $statement = $pdo->query('select * from posts');
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach( $posts as $post ){
+    echo '<li>' . $post['title']. '</li>' ;
+}
+
+die();
 require "router.php";
 
