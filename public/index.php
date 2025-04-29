521c294aa9e91d65edc7b6e61e1cb8c30e684b2d
Utkarsh Gayguwal <utkarshgayguwal.tg@gmail.com>
2025-04-29 23:41:26 +0530

    spl_autoload_register used

--- public/index.php
+++ public/index.php
@@ -4,7 +4,10 @@ const BASE_PATH = __DIR__ . "/../";
 
 
 require BASE_PATH . "function.php";
-require base_path("Database.php");
-require base_path("Response.php");
+
+spl_autoload_register(function ($class) {
+    require base_path("{$class}.php");
+});
+
 require base_path("router.php");
 
