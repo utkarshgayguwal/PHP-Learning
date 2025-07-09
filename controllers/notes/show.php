430a378ccd3efee991157fc67d2d4c3466075196
Utkarsh Gayguwal <utkarshgayguwal.tg@gmail.com>
2025-07-09 18:52:16 +0000

    Updated logic related store function where storing not happening, and validations not showing.

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,19 +6,7 @@ $heading = "Note Description";
 
 $config = require base_path('config.php');
 $db  = new Database($config['database']);
-
-
 $currentUser = 1;
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $query = "select * from notes where id = :id";
-    $note = $db->query($query, ['id' => $_POST['id']])->fetchOrAbort();
-    authorised($note['user_id'] === $currentUser);
-    $db->query("delete from notes where id = :id", ['id' => $_POST['id']]);
-    header("Location: /notes");
-    exit;
-}
-
 
 $query = "select * from notes where id = :id";
 $note = $db->query($query, ['id' => $_GET['id']])->fetchOrAbort();
